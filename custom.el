b7df02731aa9c16b502ccb764670d5160b24890e
Tristan Mills <tristan.w.mills@gmail.com>
2025-04-22 10:50:22 -0700

    Add custom.el and .gitignore

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ace-window activities auctex cape casual-avy cdlatex consult-notes
+		corfu delight denote eat ef-themes eglot-tempel
+		embark-consult fontaine gnuplot helpful inf-ruby lin
+		magit marginalia markdown-mode mu4e-column-faces
+		nerd-icons-completion nerd-icons-corfu
+		nerd-icons-dired orderless org-fragtog org-superstar
+		pdf-tools pipenv rbenv spacious-padding
+		tempel-collection vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
