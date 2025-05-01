a4846abb4c33f5836e7e93c3c321af92ef37929d
Tristan Mills <tristan.mills@rula.com>
2025-05-01 09:47:28 -0700

    Add asdf.el, use consult-denote, pin avy to melpa
    - asdf.el sets the path for asdf installed packages properly
    - consult-denote is written by prot - use instead of consult-notes for
    simplicity
    - casual-avy requires the latest verion of avy which isn't on gnu-elpa

--- custom.el
+++ custom.el
@@ -3,7 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(ace-window activities auctex avy bind-key cape casual-avy cdlatex
+		cl-generic cl-lib compat consult-denote consult-notes
+		corfu diminish eat editorconfig ef-themes eglot
+		eglot-tempel eldoc embark-consult erc
+		external-completion faceup flymake fontaine gnuplot
+		helpful idlwave inf-ruby jsonrpc let-alist lin magit
+		map marginalia markdown-mode nadvice
+		nerd-icons-completion nerd-icons-corfu
+		nerd-icons-dired ntlm orderless org org-fragtog
+		org-superstar pdf-tools peg pipenv project python
+		rbenv seq so-long soap-client spacious-padding svg
+		tempel-collection track-changes tramp vc-use-package
+		verilog-mode vertico which-key window-tool-bar xref)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
