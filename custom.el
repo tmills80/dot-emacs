9c08d13fd550643884537cfbe1e4ddec1d63a6fc
Tristan Mills <tristan.w.mills@gmail.com>
2026-04-07 13:59:23 -0700

    Add kotlin to eglot config

--- custom.el
+++ custom.el
@@ -4,18 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window activities auctex bind-key cape casual-avy cdlatex
-		cl-generic cl-lib compat consult-denote corfu diminish
-		eat editorconfig ef-themes eglot eglot-tempel eldoc
-		embark-consult erc external-completion faceup flymake
-		fontaine gnuplot helpful idlwave inf-ruby jsonrpc
-		let-alist lin magit map marginalia markdown-mode
-		mu4e-column-faces nadvice nerd-icons-completion
-		nerd-icons-corfu nerd-icons-dired ntlm orderless org
-		org-fragtog org-superstar pdf-tools peg pipenv project
-		python rbenv seq so-long soap-client spacious-padding
-		svg tempel-collection track-changes tramp verilog-mode
-		vertico web-mode which-key window-tool-bar xref)))
+   '(ace-window activities ai-code-interface auctex breadcrumb cape
+		casual-avy cdlatex claude-code claude-code-ide
+		combobulate consult-denote corfu csv-mode eat
+		ef-themes eglot-tempel elfeed-goodies elfeed-org
+		emacs-everywhere embark-consult flymake-eslint
+		fontaine gnuplot helpful inf-ruby jest-test-mode
+		jsonnet-mode kotlin-ts-mode lin magit-popup marginalia
+		markdown-mode mu4e-column-faces nerd-icons-completion
+		nerd-icons-corfu nerd-icons-dired orderless
+		org-fragtog org-superstar pdf-tools pipenv smartparens
+		spacious-padding tempel-collection terraform-mode
+		treesit-fold vertico vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
